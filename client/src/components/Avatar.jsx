48745a9cde8be3581202b24b9cab014f0ba11159
Hirun-Harshana-234 <hirunharshanai@gmail.com>
2026-08-31 18:26:12 +0530

    Delete client directory

--- client/src/components/Avatar.jsx
+++
@@ -1,9 +0,0 @@
-export default function Avatar({ user, size = "medium", showStatus = false, online = false }) {
-  const initials = (user?.displayName || user?.username || "?").split(/\s+/).map((part) => part[0]).join("").slice(0, 2).toUpperCase();
-  return (
-    <span className={`avatar avatar-${size} ${user?.avatarData ? "has-image" : ""}`} style={{ background: user?.avatarColor || "#3d73ff" }} title={user?.displayName || user?.username}>
-      {user?.avatarData ? <img src={user.avatarData} alt="" /> : initials}
-      {showStatus && <span className={`presence-dot ${online ? "online" : ""}`} />}
-    </span>
-  );
-}
--- client/src/test/setup.js
+++
@@ -1,5 +0,0 @@
-import "@testing-library/jest-dom";
-
-Object.defineProperty(global.navigator, "onLine", { value: true, configurable: true });
-if (!global.crypto.randomUUID) global.crypto.randomUUID = () => "test-uuid";
-
